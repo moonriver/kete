2174d8d657214f28c5e9662845977567ccd429a0
Kieran Pilkington <kieran@katipo.co.nz>
2008-08-26 11:22:02 +1200

    refinement: refined baskets tests, enhanced functional test conveniance methods, applied to licenses and topic types controller

--- test/functional/baskets_controller_test.rb
+++ test/functional/baskets_controller_test.rb
@@ -15,58 +15,74 @@ class BasketsControllerTest < ActionController::TestCase
     login_as(:admin)
 
     # hash of params to create new instance of model, e.g. {:name => 'Test Model', :description => 'Dummy'}
+    @default_model = { :show_privacy_controls => nil,
+                       :private_default => nil,
+                       :file_private_default => nil,
+                       :allow_non_member_comments => nil }
     @new_model =     { :name => 'Test Basket',
                        :show_privacy_controls => true,
                        :private_default => false,
                        :file_private_default => nil,
                        :allow_non_member_comments => true }
-    # hash of params to update existing instance of model, e.g. {:name => 'Test Model', :description => 'Dummy'}
     @updated_model = { :show_privacy_controls => nil,
                        :private_default => true,
                        :file_private_default => false,
                        :allow_non_member_comments => nil }
   end
 
+  def test_redirect_to_basket_all
+    get :show, :urlified_name => 'site'
+    assert_redirect_to({ :urlified_name => 'site', :controller_name_for_zoom_class => 'topics' })
+  end
+
+  def test_index_and_list
+    get :index, index_path
+    assert_viewing_template 'baskets/list'
+    assert_var_assigned true
+    assert_equal 4, assigns(:baskets).size
+
+    get :list, index_path({ :action => 'list' })
+    assert_viewing_template 'baskets/list'
+    assert_var_assigned true
+    assert_equal 4, assigns(:baskets).size
+  end
+
   def test_new
     get :new, new_path
-    assert_response :success
-    assert_template 'baskets/new'
-    assert_var_assigned?
-    assert_equal nil, assigns(:basket).show_privacy_controls
-    assert_equal nil, assigns(:basket).private_default
-    assert_equal nil, assigns(:basket).file_private_default
-    assert_equal nil, assigns(:basket).allow_non_member_comments
+    assert_viewing_template 'baskets/new'
+    assert_var_assigned
+    assert_attributes_same_as @default_model
   end
 
   def test_create
     create_record
-    assert_var_assigned?
-    assert_equal true, assigns(:basket).show_privacy_controls
-    assert_equal false, assigns(:basket).private_default
-    assert_equal nil, assigns(:basket).file_private_default
-    assert_equal true, assigns(:basket).allow_non_member_comments
-    assert_equal false, assigns(:basket).new_record?
-    assert_response :redirect
-    assert_redirected_to edit_path({ :urlified_name => assigns(:basket).urlified_name, :id => assigns(:basket).id })
+    assert_var_assigned
+    assert_attributes_same_as @new_model
+    assert_redirect_to( edit_path({ :urlified_name => assigns(:basket).urlified_name, :id => assigns(:basket).id }) )
+    assert_equal 'Basket was successfully created.', flash[:notice]
   end
 
   def test_edit
     get :edit, edit_path
-    assert_response :success
-    assert_template 'baskets/edit'
-    assert_var_assigned?
+    assert_viewing_template 'baskets/edit'
+    assert_var_assigned
+
+    get :homepage_options, edit_path({ :action => 'homepage_options' })
+    assert_viewing_template 'baskets/homepage_options'
+    assert_var_assigned
   end
 
   def test_update
     update_record
-    assert_var_assigned?
-    assert_equal nil, assigns(:basket).show_privacy_controls
-    assert_equal true, assigns(:basket).private_default
-    assert_equal false, assigns(:basket).file_private_default
-    assert_equal nil, assigns(:basket).allow_non_member_comments
-    assert_response :redirect
-    assert_redirected_to :urlified_name => 'site'
+    assert_var_assigned
+    assert_attributes_same_as @updated_model
+    assert_redirect_to({ :urlified_name => 'site' })
     assert_equal 'Basket was successfully updated.', flash[:notice]
   end
 
+  def test_destroy
+    destroy_record({ :id => 4 }) # documentation basket
+    assert_redirect_to '/'
+    assert_equal 'Basket was successfully deleted.', flash[:notice]
+  end
 end
--- test/functional/licenses_controller_test.rb
+++ test/functional/licenses_controller_test.rb
@@ -1,79 +1,70 @@
 require File.dirname(__FILE__) + '/../test_helper'
 require 'licenses_controller'
 
+# Re-raise errors caught by the controller.
 class LicensesController; def rescue_action(e) raise e end; end
 
 class LicensesControllerTest < ActionController::TestCase
+
+  include KeteTestFunctionalHelper::TestHelper
   include AuthenticatedTestHelper
 
   fixtures :licenses
 
   def setup
-    @site_basket = Basket.find(:first)
-    login_as :admin
+    @base_class = "License"
+    @assignment_var = "record" # singular, lowercase
+    load_test_environment
+    login_as(:admin)
+
+    # hash of params to create new instance of model, e.g. {:name => 'Test Model', :description => 'Dummy'}
+    @new_model =     { :name => 'License1',
+                       :description => 'License1' }
+    @updated_model = { :name => 'License2',
+                       :description => 'License2' }
   end
 
-  def test_should_get_index
-    get :index, :urlified_name => 'site', :controller => 'licenses'
-    assert_response :success
-    assert_not_nil assigns(:records)
+  def test_index
+    get :index, index_path
+    assert_viewing_template 'licenses/list'
+    assert_var_assigned true
+    assert_equal 4, assigns(:records).size
   end
 
-  def test_should_get_new
-    get :new, :urlified_name => 'site'
-    assert_response :success
+  def test_show
+    get :show, show_path({ :id => licenses(:one).id })
+    assert_viewing_template 'licenses/show'
+    assert_var_assigned 
   end
 
-  # Kieran Pilkington, 2008/08/11
-  # This will fail because we currently have no validations in place to make sure something is entered
-  #def test_shouldnt_create_license
-  #  assert_no_difference('License.count') do
-  #    post :create, {:urlified_name => 'site', :record => {}}
-  #  end
-  #end
-
-  def test_should_create_license
-    assert_difference('License.count') do
-      post :create, {:urlified_name => 'site', :record => { :record_name => 'test', :record_description => 'test' }}
-    end
-
-    assert_redirected_to license_path(assigns(:license))
+  def test_new
+    get :new, new_path
+    assert_viewing_template 'licenses/create_form'
+    assert_var_assigned
   end
 
-  def test_should_show_license
-    get :show, :id => licenses(:one).id, :urlified_name => 'site'
-    assert_response :success
+  def test_create
+    create_record
+    assert_var_assigned
+    assert_attributes_same_as @new_model
+    assert_redirect_to( index_path )
   end
 
-  def test_should_get_edit
-    get :edit, :id => licenses(:one).id, :urlified_name => 'site'
-    assert_response :success
+  def test_edit
+    get :edit, edit_path({ :id => licenses(:one).id })
+    assert_viewing_template 'licenses/update_form'
+    assert_var_assigned
   end
 
-  def test_should_update_license
-    put :update, {:id => licenses(:one).id, :urlified_name => 'site', :record => { :record_name => 'test', :record_description => 'test' }}
-    assert_redirected_to license_path(assigns(:license))
+  def test_update
+    update_record( {}, { :id => licenses(:one).id } )
+    assert_var_assigned
+    assert_attributes_same_as @updated_model
+    assert_redirect_to( index_path )
   end
 
-  def test_should_destroy_license
-    assert_difference('License.count', -1) do
-      delete :destroy, :id => licenses(:one).id, :urlified_name => 'site'
-    end
-
-    assert_redirected_to licenses_path
-  end
-
-  private
-
-  def license_path(options = {})
-    license_path_hash = { :urlified_name => 'site', :controller => 'licenses' }
-    license_path_hash = license_path_hash.merge(options) unless options.nil?
-    license_path_hash
-  end
-
-  def licenses_path(options = {})
-    license_path_hash = { :urlified_name => 'site', :controller => 'licenses' }
-    license_path_hash = license_path_hash.merge(options) unless options.nil?
-    license_path_hash
+  def test_destroy
+    destroy_record({ :id => licenses(:one).id })
+    assert_redirect_to( index_path )
   end
 end
